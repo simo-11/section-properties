28a418e13245a5133cd834151248b4d71730d4f9
Simo Nikula <simo.nikula@gmail.com>
2023-03-04 20:30:55 +0200

    comments added to sample_cells.py

--- examples/11-simo/sample_cells.py
+++ examples/11-simo/sample_cells.py
@@ -2,6 +2,12 @@
 """
 Created on Tue Feb 21 19:42:59 2023
 
+This file is used to speedup testing of various fragments of code
+known as cells e.g. in Spyder which provides means to run and debug cells.
+Matlab has quite similar feature for matlab-scripts but uses
+%% as cell separator
+
+
 @author: snikula
 """
 # %% initialize rectangle using normal run
@@ -14,7 +20,14 @@ import builtins
 builtins.debugfile(
     'C:/github/section-properties/examples/11-simo/rectangle.py',
         args='--plot_warping_values -A -W 1 -H 4 --mesh_size=0.05')
-# %%% torsion vector plot from upstream
+# %%% torsion stress vector plot from upstream
+"""
+Note to myself, figure details on how warping function values (at nodes)
+are used with shape function derivates (at gauss integration points)
+to get correct shear stresses.
+https://sectionproperties.readthedocs.io/en/latest/rst/theory.html
+refers to usage of smoothing matrix.
+"""
 stress_post = section.calculate_stress(Mzz=1e6)#noqa
 stress_post.plot_vector_mzz_zxy()
 # %%% warping
