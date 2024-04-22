4901f5646da1c8dd4a0dedf263def016ccd48d87
Simo Nikula <simo.nikula@gmail.com>
2024-04-22 19:42:54 +0300

    paper_cells.py continued

--- examples/11-simo/paper_cells.py
+++ examples/11-simo/paper_cells.py
@@ -29,6 +29,28 @@ for i in range(0, n):
         print('Wrote {0}'.format(fn))
     section.write_warping_csv()
     section.write_warping_gltf()
+# %% rectangle 100-10
+import matplotlib.pyplot as plt
+mesh_sizes=(3e-4, 1e-4, 3e-5)
+n=len(mesh_sizes)
+for i in range(0, n):
+    section=None
+    runfile('primitive.py',#noqa
+      args="""-A -W=0.1 -H=0.01 
+      --mesh_size={0} 
+      --primitive=rectangle""".format(mesh_sizes[i]))
+    if i == 0:
+        pdf=plt.figure()
+        fn=section.gfn(section.default_filename(".pdf","geometry"))
+        section.geometry.plot_geometry(
+            labels=()
+            ,title="solid square 100x100 mm"
+            ,cp=False
+            ,legend=False
+            ,filename=fn)
+        print('Wrote {0}'.format(fn))
+    section.write_warping_csv()
+    section.write_warping_gltf()
 # %%
 
 
